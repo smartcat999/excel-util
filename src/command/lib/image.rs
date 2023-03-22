feaf570e411722c56bc71de70a2ca111a2bc91a3
peng wu <2030047311@qq.com>
2023-03-22 11:09:51 +0800

    fix: update output string trim_matches

--- src/command/lib/image.rs
+++ src/command/lib/image.rs
@@ -26,7 +26,7 @@ impl Cmd for InspectCmd {
     }
 
     fn handler(&self, s: &str) {
-        let s = s.trim_matches(|c| c == '[' || c == ']' || c == ' ');
+        let s = s.trim_matches(|c| c == '[' || c == ']' || c == ' ' || c == '\n' || c == '\r');
         println!("s: {}", s);
     }
 }
@@ -37,4 +37,17 @@ pub fn run_cmd(cmd: Box<dyn Cmd>) {
     let out = String::from_utf8(output.stdout).unwrap();
     println!("cmd: {}\noutput: {}", input, &out);
     cmd.handler(&out);
+}
+
+
+#[cfg(test)]
+mod test {
+    #[allow(unused_imports)]
+    use super::*;
+
+    #[test]
+    fn test_trim() {
+        let s = "[sha256:fc251a6e798157dc3b46fd265da72f39cd848e3f9f4a0b28587d1713b878deb9 sha256:750218a4877112ef906ed0f0769642a12ffde1c6fed7ddebe3fe2090634e8684 sha256:57da6125df86d6738c5188290d51b700ee11ba2bc3ede18d2d415b2b33f70a36]";
+        println!("{}", s.trim_matches(|c| c == '[' || c == ']' || c == ' '));
+    }
 }
